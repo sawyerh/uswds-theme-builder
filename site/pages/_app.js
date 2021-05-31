09224fc80e0bf93b17889633175c53fae42af431
Sawyer <git@sawyerh.com>
2021-05-31 12:42:59 -0400

    Add site title

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles/app.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Theme builder for U.S. Web Design System (USWDS)</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;

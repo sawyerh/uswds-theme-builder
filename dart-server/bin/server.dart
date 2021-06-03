b8f3a3f2a5d274cbd046eaed89ad031ac261335d
Sawyer <git@sawyerh.com>
2021-06-02 22:50:59 -0400

    Remove unused files

--- dart-server/bin/server.dart
+++ dart-server/bin/server.dart
@@ -8,17 +8,14 @@ import 'dart:io';
 import 'package:sass/sass.dart' as sass;
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
-import 'package:shelf_static/shelf_static.dart' as shelf_static;
 
 Future main() async {
   // If the "PORT" environment variable is set, listen to it. Otherwise, 8080.
   // https://cloud.google.com/run/docs/reference/container-contract#port
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
-  final cascade = Cascade().add(_staticHandler).add(_compileHandler);
-
   final pipeline =
-      Pipeline().addMiddleware(logRequests()).addHandler(cascade.handler);
+      Pipeline().addMiddleware(logRequests()).addHandler(_compileHandler);
 
   final server = await shelf_io.serve(
     pipeline,
@@ -28,10 +25,6 @@ Future main() async {
 
   print('Serving at http://${server.address.host}:${server.port}');
 }
-
-// Serve files from the file system.
-final _staticHandler =
-    shelf_static.createStaticHandler('public', defaultDocument: 'index.html');
 
 Future<Response> _compileHandler(Request request) async {
   var bodyRaw = await request.readAsString();

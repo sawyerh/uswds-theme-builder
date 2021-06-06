2887ff8b10362a132ebc2283292887ecda7ec54a
Sawyer <git@sawyerh.com>
2021-06-06 12:08:00 -0400

    Cleanup Dart server

--- dart-sass-server/bin/server.dart
+++ dart-sass-server/bin/server.dart
@@ -9,8 +9,10 @@ Future main() async {
   // https://cloud.google.com/run/docs/reference/container-contract#port
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
-  final pipeline =
-      Pipeline().addMiddleware(logRequests()).addHandler(_compileHandler);
+  final pipeline = Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(createMiddleware(responseHandler: _setDefaultHeaders))
+      .addHandler(_requestHandler);
 
   final server = await shelf_io.serve(
     pipeline,
@@ -23,33 +25,60 @@ Future main() async {
   print('Serving at http://${server.address.host}:${server.port}');
 }
 
-Future<Response> _compileHandler(Request request) async {
-  const corsHeaders = {'Access-Control-Allow-Origin': '*'};
+Response _setDefaultHeaders(Response response) {
+  final corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
+  };
 
+  return response.change(headers: corsHeaders);
+}
+
+Future<Response> _requestHandler(Request request) async {
   if (request.method == 'OPTIONS') {
-    return Response.ok(null, headers: corsHeaders);
+    return Response.ok(null);
   }
 
+  var sassContents = await createSassContents(request);
+
+  try {
+    var output = sass.compileString(sassContents,
+        loadPaths: ['bin'],
+        quietDeps: true,
+        style: sass.OutputStyle.compressed);
+
+    return Response.ok(output, headers: {'Content-Type': 'text/css'});
+  } on sass.SassException catch (e) {
+    return Response(400, body: e.toCssString());
+  }
+}
+
+Future<String> createSassContents(Request request) async {
   var bodyRaw = await request.readAsString();
   var body = bodyRaw.isNotEmpty ? json.decode(bodyRaw) : {};
-  var tokens = [];
-  body.forEach((key, value) => tokens.add('$key: $value;'));
+  var sassVars = [];
+  body.forEach((key, value) => sassVars.add('$key: $value;'));
 
-  var source = '''
+  return '''
   /*! USWDS Theme Builder */
 
-  /* Fix uswds themeing in dart sass */
+  /*
+    Fix USWDS themeing in Dart Sass:
+    https://github.com/uswds/uswds/issues/4220
+  */
   @use "sass:math" as *;
 
+  /*
+    Make the Sass compilation log less noisy
+  */
   \$theme-show-notifications: false;
-  ${tokens.join('')}
+  \$theme-show-compile-warnings: false;
+
+  /*
+    Custom Sass variables passed in through the request
+  */
+  ${sassVars.join('')}
 
   @import "../public/uswds/uswds.scss";
 ''';
-
-  var output = sass.compileString(source,
-      quietDeps: true, style: sass.OutputStyle.compressed);
-
-  return Response.ok(output,
-      headers: {'Content-Type': 'text/css', ...corsHeaders});
 }

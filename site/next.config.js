8a571e62cbbc58cda8f8f1cf82f67d30f71a1316
Sawyer <git@sawyerh.com>
2021-06-26 17:43:09 -0400

    Move preview HTML to own file

--- site/next.config.js
+++ site/next.config.js
@@ -5,4 +5,16 @@ module.exports = {
     includePaths: [path.resolve(__dirname, "node_modules/uswds/dist/scss")],
   },
   trailingSlash: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.html$/i,
+      loader: "html-loader",
+      options: {
+        // Don't attempt to import img files
+        sources: false,
+      },
+    });
+
+    return config;
+  },
 };

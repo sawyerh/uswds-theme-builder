d96991929e476dd19ce12deb9bde8fe92c1ed441
Sawyer <git@sawyerh.com>
2021-06-26 19:55:06 -0400

    Add TokensEditor component

---
+++ site/components/TokensEditor.js
@@ -0,0 +1,97 @@
+import {
+  ArticleNyTimes,
+  Drop,
+  FrameCorners,
+  PuzzlePiece,
+} from "phosphor-react";
+import ColorTokens from "../components/ColorTokens";
+import { useEffect } from "react";
+import useTokensManager from "../hooks/useTokensManager";
+
+const TokensEditor = ({ previewIframeRef }) => {
+  const tokensManager = useTokensManager();
+
+  /**
+   * Send tokens into our Preview iFrame when they change
+   */
+  useEffect(() => {
+    previewIframeRef.current.contentWindow.postMessage(
+      tokensManager.customTokens
+    );
+  }, [tokensManager.customTokens, previewIframeRef]);
+
+  return (
+    <form
+      onSubmit={(e) => e.preventDefault}
+      className="padding-x-2 padding-y-3 usa-form maxw-none"
+    >
+      <h2 className="text-white font-sans-lg margin-bottom-1 margin-top-0">
+        <span className="display-inline-block text-middle margin-right-05 text-mint">
+          <Drop size={30} />
+        </span>
+        Colors
+      </h2>
+      <a
+        className="text-white display-inline-block margin-bottom-2"
+        href="https://designsystem.digital.gov/design-tokens/color/theme-tokens/"
+        target="_blank"
+      >
+        Learn about color tokens.
+      </a>
+
+      <div className="margin-bottom-4">
+        <ColorTokens tokensManager={tokensManager} />
+      </div>
+
+      <h2 className="text-white font-sans-lg margin-bottom-0">
+        <span className="display-inline-block text-middle margin-right-1 text-gold">
+          <ArticleNyTimes size={30} weight="regular" />
+        </span>
+        Typography
+      </h2>
+      {/* <a
+    className="text-white display-inline-block margin-bottom-2"
+    href="https://designsystem.digital.gov/design-tokens/typesetting/overview/"
+    target="_blank"
+  >
+    Learn about typesetting tokens.
+  </a> */}
+      <a
+        href="https://forms.gle/adA3KkTjxqHcsH5S8"
+        className="text-white font-body-3xs"
+      >
+        Get notified when this functionality is ready.
+      </a>
+
+      <h2 className="text-white font-sans-lg  margin-bottom-0">
+        <span className="display-inline-block text-middle margin-right-1 text-cyan">
+          <FrameCorners size={30} />
+        </span>
+        Spacing
+      </h2>
+
+      <a
+        href="https://forms.gle/adA3KkTjxqHcsH5S8"
+        className="text-white font-body-3xs"
+      >
+        Get notified when this functionality is ready.
+      </a>
+
+      <h2 className="text-white font-sans-lg  margin-bottom-0">
+        <span className="display-inline-block text-middle margin-right-1 text-orange">
+          <PuzzlePiece size={30} />
+        </span>
+        Components
+      </h2>
+
+      <a
+        href="https://forms.gle/adA3KkTjxqHcsH5S8"
+        className="text-white font-body-3xs"
+      >
+        Get notified when this functionality is ready.
+      </a>
+    </form>
+  );
+};
+
+export default TokensEditor;
--- site/pages/index.js
+++ site/pages/index.js
@@ -1,20 +1,11 @@
-import {
-  ArticleNyTimes,
-  Clipboard,
-  Drop,
-  FilePlus,
-  FrameCorners,
-  PuzzlePiece,
-  Sliders,
-} from "phosphor-react";
-import { useEffect, useRef, useState } from "react";
-import ColorTokens from "../components/ColorTokens";
+import { Clipboard, FilePlus, Sliders } from "phosphor-react";
+import { useRef, useState } from "react";
 import IconButton from "../components/IconButton";
 import classnames from "classnames";
-import useTokensManager from "../hooks/useTokensManager";
 import CodeEditor from "../components/CodeEditor";
 import Header from "../components/Header";
 import Head from "next/head";
+import TokensEditor from "../components/TokensEditor";
 
 if (typeof window !== "undefined") {
   require("codemirror/mode/xml/xml");
@@ -37,17 +28,7 @@ const navButtons = [
 
 export default function Home() {
   const previewIframeRef = useRef();
-  const tokensManager = useTokensManager();
   const [activeNavButton] = useState(navButtons[0]);
-
-  /**
-   * Send tokens into our Preview iFrame when they change
-   */
-  useEffect(() => {
-    previewIframeRef.current.contentWindow.postMessage(
-      tokensManager.customTokens
-    );
-  }, [tokensManager.customTokens, previewIframeRef]);
 
   return (
     <>
@@ -90,76 +71,7 @@ export default function Home() {
               </IconButton>
             ))}
           </nav>
-          <form
-            onSubmit={(e) => e.preventDefault}
-            className="padding-x-2 padding-y-3 usa-form maxw-none"
-          >
-            <h2 className="text-white font-sans-lg margin-bottom-1 margin-top-0">
-              <span className="display-inline-block text-middle margin-right-05 text-mint">
-                <Drop size={30} />
-              </span>
-              Colors
-            </h2>
-            <a
-              className="text-white display-inline-block margin-bottom-2"
-              href="https://designsystem.digital.gov/design-tokens/color/theme-tokens/"
-              target="_blank"
-            >
-              Learn about color tokens.
-            </a>
-
-            <div className="margin-bottom-4">
-              <ColorTokens tokensManager={tokensManager} />
-            </div>
-
-            <h2 className="text-white font-sans-lg margin-bottom-0">
-              <span className="display-inline-block text-middle margin-right-1 text-gold">
-                <ArticleNyTimes size={30} weight="regular" />
-              </span>
-              Typography
-            </h2>
-            {/* <a
-              className="text-white display-inline-block margin-bottom-2"
-              href="https://designsystem.digital.gov/design-tokens/typesetting/overview/"
-              target="_blank"
-            >
-              Learn about typesetting tokens.
-            </a> */}
-            <a
-              href="https://forms.gle/adA3KkTjxqHcsH5S8"
-              className="text-white font-body-3xs"
-            >
-              Get notified when this functionality is ready.
-            </a>
-
-            <h2 className="text-white font-sans-lg  margin-bottom-0">
-              <span className="display-inline-block text-middle margin-right-1 text-cyan">
-                <FrameCorners size={30} />
-              </span>
-              Spacing
-            </h2>
-
-            <a
-              href="https://forms.gle/adA3KkTjxqHcsH5S8"
-              className="text-white font-body-3xs"
-            >
-              Get notified when this functionality is ready.
-            </a>
-
-            <h2 className="text-white font-sans-lg  margin-bottom-0">
-              <span className="display-inline-block text-middle margin-right-1 text-orange">
-                <PuzzlePiece size={30} />
-              </span>
-              Components
-            </h2>
-
-            <a
-              href="https://forms.gle/adA3KkTjxqHcsH5S8"
-              className="text-white font-body-3xs"
-            >
-              Get notified when this functionality is ready.
-            </a>
-          </form>
+          <TokensEditor previewIframeRef={previewIframeRef} />
         </section>
         <div className="grid-col-fill display-flex flex-column">
           <section className="bg-base-lighter flex-fill position-relative">
